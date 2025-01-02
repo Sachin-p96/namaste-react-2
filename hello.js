5cfaa61db610805bb383e20477f0e17af0a1f2a6
sachin p <sachin.p06@infosys.com>
2025-01-02 11:39:14 +0530

    added parcel

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = [
   React.createElement("h1", {}, "Hello We are react enginners"),
   React.createElement("h4", {}, "heybaba"),
