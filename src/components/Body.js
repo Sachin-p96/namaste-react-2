26f1a0f1bb379c3b6f7a923325d4940749503111
sachin p <sachin.p06@infosys.com>
2025-01-10 15:23:12 +0530

    added changes for calling api,useffect,usestate

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,30 @@ import React, { useEffect, useState } from "react";
 import { RestoCard } from "./RestoCard";
 import { restoDetails } from "../utils/constants";
 import SearchBar from "./SearchBar";
+import { Image, Breathing } from "react-shimmer";
+import {
+  ShimmerFeaturedGallery,
+  ShimmerSimpleGallery,
+} from "react-shimmer-effects";
 
 export const Body = () => {
-  const [filterRest, setFilterRest] = useState(restoDetails);
-
+  const [filterRest, setFilterRest] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.854899&lng=77.6679292&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const dataJson = await data.json();
+    console.log(
+      dataJson.data.cards[1].card.card.gridElements.infoWithStyle.restaurants,
+      "heyData"
+    );
+    setFilterRest(
+      dataJson.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
   const handleClick = (value) => {
     console.log("Hey Im Clicked", value);
     const filteredList = restoDetails.filter((item) => {
@@ -14,6 +34,13 @@ export const Body = () => {
     setFilterRest(filteredList);
   };
   console.log(filterRest);
+  if (filterRest.length === 0) {
+    return (
+      <div>
+        <ShimmerFeaturedGallery row={2} col={2} card frameHeight={600} />
+      </div>
+    );
+  }
   return (
     <div className="body">
       <div className="search-container">
